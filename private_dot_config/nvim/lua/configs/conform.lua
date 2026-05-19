20fb208263c13d08a537835af758ce5f43b1416c
Eshwar Kancherla <eshwar.kancherla@verkada.com>
2026-05-19 12:51:11 -0700

    Added shell formatter shfmt

--- private_dot_config/nvim/lua/configs/conform.lua
+++ private_dot_config/nvim/lua/configs/conform.lua
@@ -1,29 +1,35 @@
 local options = {
-  formatters_by_ft = {
-    lua = { "stylua" },
-    json = { "jq" },
-    python = { 
-      "ruff_fix",
-      extra_args = { "--lint-unfixable", "F401" },
-    },
-    sql = {
-      "sql_formatter",
-    }
-    -- css = { "prettier" },
-    -- html = { "prettier" },
-  },
+	formatters_by_ft = {
+		lua = { "stylua" },
+		json = { "jq" },
+		python = {
+			"ruff_fix",
+			extra_args = { "--lint-unfixable", "F401" },
+		},
+		sql = {
+			"sql_formatter",
+		},
+		sh = { "shfmt" },
+		bash = { "shfmt" },
+		zsh = { "shfmt" },
+		-- css = { "prettier" },
+		-- html = { "prettier" },
+	},
 
-  formatters = {
-    sql_formatter = {
-      args = { "-c" , '{"keywordCase": "upper", "tabWidth": "4"}' },
-    },
-  },
+	formatters = {
+		sql_formatter = {
+			args = { "-c", '{"keywordCase": "upper", "tabWidth": "4"}' },
+		},
+		shfmt = {
+			prepend_args = { "-i", "4", "-ci", "-ln=auto", "-d", "--severity=warning" },
+		},
+	},
 
-  format_on_save = {
-    -- These options will be passed to conform.format()
-    timeout_ms = 500,
-    lsp_fallback = true,
-  },
+	format_on_save = {
+		-- These options will be passed to conform.format()
+		timeout_ms = 500,
+		lsp_fallback = true,
+	},
 }
 
 return options
