64bbda9144635a32253bae4eb4d11870e456d468
esh-k <esh.eshwar.eshwar.esh@gmail.com>
2026-06-07 19:25:37 -0700

    Added transparency

--- private_dot_config/nvim/lua/chadrc.lua
+++ private_dot_config/nvim/lua/chadrc.lua
@@ -1,16 +1,17 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
 local M = {}
 
 M.base46 = {
-	theme = "catppuccin",
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  theme = "catppuccin",
+  transparency = true,
+  -- hl_override = {
+  -- 	Comment = { italic = true },
+  -- 	["@comment"] = { italic = true },
+  -- },
 }
 
 M.term = { float = { true } }
@@ -18,7 +19,7 @@ M.nvimtree = {
   view = {
     preserve_window_proportions = true,
     adaptive_size = true,
-  }
+  },
 }
 -- M.nvdash = { load_on_startup = true }
 -- M.ui = {
